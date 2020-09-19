af52f35c94d505766252b7b3d57a8b4b35d2da3c
MineCommanderCN <59791306+MineCommanderCN@users.noreply.github.com>
2020-09-19 21:58:16 +0800

    Update demo.cpp

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,18 +1,25 @@
 #include "squidCore_lib.hpp"
 
-int printHello(argsAry args) {
-	std::cout << "Hello world!" << std::endl;
+int printHello(const squidcore::argsAry &args) {
+	squidcore::io.print("Hello world!");
+	return 0;
+}
+
+int aplusb(const squidcore::argsAry &args){
+	float a = squidcore::str2num(args[1]), b = squidcore::str2num(args[2]);
+	squidcore::io.print(squidcore::atob<float, std::string>(a+b));
 	return 0;
 }
 
 
-
 void regist_cmd(void) {
 	squidcore::regcmd("test",printHello,1,1);
+	squidcore::regcmd("a+b",aplusb,3,3);
 }
 
 
 int main() {
+	squidcore::io.coloringOnLinux(false);
 	regist_cmd();
 	while (1) {
 		std::string input;
