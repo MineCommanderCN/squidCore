943df81b7989e5a121060f55dc2564cfcdbb8771
MineCommanderCN <minecommander@outlook.com>
2020-10-11 10:55:09 +0800

    v0.1.5

--- src/squidCore_lib.hpp
+++ src/squidCore_lib.hpp
@@ -1,10 +1,25 @@
-/*
-*   SquidCore v0.1.4
+﻿/*
+*   SquidCore v0.1.5
 *   Copyright (C) MineCommander 2020, use MIT License
 *   Repo: https://github.com/MineCommanderCN/squidCore
 */
+
+/*
+If you want to use the macro to build squidCore as a .dll file, please make sure:
+  1. You are using Visual Studio as the IDE;
+  2. Your operating system is Windows;
+  3. Using the "Dynamic link library" template to generate the project.
+Then, remove the comment of the following code:
+*/
+      #define SQUIDCORE_EXPORT_AS_DLL true
+
+#ifdef SQUIDCORE_EXPORT_AS_DLL
+    #define USE_DLL _declspec(dllexport)
+#else
+    #define USE_DLL
+#endif
+#pragma warning(disable:4996)
 #pragma once
-#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<vector>
 #include<map>
@@ -14,9 +29,11 @@
 #include<sstream>
 
 namespace sqc {
-	typedef std::vector<std::string> argsAry;
-    typedef int(*Fp)(const argsAry &args);
+    typedef std::vector<std::string> argsAry;
+    typedef int(*Fp)(const argsAry& args);
     const int EXIT_MAIN = 65536;
+    const int UNKNOWN_COMMAND = 65537;
+    const int ARGUMENT_ERROR = 65538;
     const int MAXN = 2147483647;
     struct tCmdreg {
         Fp func;
@@ -25,42 +42,41 @@ namespace sqc {
     };
     std::map<std::string, tCmdreg> cmd_register;    //first -> root command, second -> command info
     template <class Ta, class Tb>
-    Tb atob(const Ta& t) {    //strstream-based type conversion (such as std::string->float)
+    Tb USE_DLL atob(const Ta& t) {    //strstream-based type conversion (such as std::string->float)
         std::stringstream temp;
         temp << t;
         Tb i;
         temp >> i;
         return i;
     }
-    double str2num(std::string str){    //A shortcut of atob<std::string, double>(), which is convenient for most use
-    	return atob<std::string, double>(str);
+    double USE_DLL str2num(std::string str) {    //A shortcut of atob<std::string, double>(), which is convenient for most use
+        return atob<std::string, double>(str);
     }
-    std::string num2str(double num){    //A shortcut of atob<double, std::string>(), which is convenient for most use
-    	return atob<double, std::string>(num);
+    std::string USE_DLL num2str(double num) {    //A shortcut of atob<double, std::string>(), which is convenient for most use
+        return atob<double, std::string>(num);
     }
-    argsAry compile(std::string str) {
+    argsAry USE_DLL compile(std::string str) {
         str += "\n";
         argsAry tmp;
         std::string strtmp;
-        int state = 0;
+        int state = 0;  /*  state==0 scanning blank chars (spqce)
+                            state==1 scanning strings between two spaces
+                            state==2 scanning strings between two quotes
+                        */
         for (std::string::iterator ii = str.begin(); ii != str.end(); ii++) {
             if (*ii == '\n') {
                 tmp.push_back(strtmp);
                 return tmp;
             }
-            if (state == 0) {
-                if (*ii == ' ') {
 
-                }
-                else if (*ii == '"') {
-                    state = 2;
-                }
-                else {
-                    state = 1;
-                    strtmp.push_back(*ii);
-                }
-            }
-            else if (state == 1) {
+            switch (state) {
+            case 0:
+                switch (*ii) {
+                case ' ': break;
+                case '"': state = 2; break;
+                default: state = 1; strtmp.push_back(*ii);
+                } break;
+            case 1:
                 if (*ii == ' ') {
                     tmp.push_back(strtmp);
                     strtmp.clear();
@@ -68,114 +84,60 @@ namespace sqc {
                 }
                 else {
                     strtmp.push_back(*ii);
-                }
-            }
-            else if (state == 2) {
+                } break;
+            case 2:
                 if (*ii == '"') {
-                    tmp.push_back(strtmp);
-                    strtmp.clear();
-                    state = 0;
+                    strtmp = "\"" + strtmp;
+                    strtmp.push_back(*ii);
+                    state = 1;
                 }
                 else {
                     strtmp.push_back(*ii);
-                }
+                } break;
+            default:
+                throw "squidCore: Unexpected error at command parsing";
             }
+
         }
         return tmp;
     }
-    class t_ioClass {
-    	private:
-    	    bool logWithTime = false;
-    	    bool coloringLinuxConsole = false;
-    	    std::string getSysTime(void) {
-                time_t t = time(0);
-                char tmp[16];
-                strftime(tmp, sizeof(tmp), "[%H:%M:%S] ", localtime(&t));
-                return tmp;
-    	    }
-    	public:
-    	    const short INFO = 1;
-    	    const short WARNING = 2;
-    	    const short ERROR = 3;
-    	    const short FATAL_ERROR = 4;
-    	    const short DEBUG = 5;
-    	    int print(std::string buf) {
-    	    	std::cout << ((logWithTime)? getSysTime()+buf : buf) << std::endl;
-    	    	return 0;
-    	    }
-    	    int printWithPrefix(std::string buf, short type) {
-    	    	std::string prefix;
-    	    	if (coloringLinuxConsole) {
-    	    		switch (type) {
-    	    			case 5: prefix += "\033[34m"; break;
-    	    			case 4: prefix += "\033[41m\033[37m"; break;
-    	    			case 3: prefix += "\033[31m"; break;
-    	    			case 2: prefix += "\033[33m"; break;
-    	    			case 1: prefix += "\033[0m"; break;
-    	    		}
-    	    	}
-    	    	switch (type) {
-    	    		case 5: prefix += "[DEBUG]"; break;
-    	    		case 4: prefix += "[FATAL_ERROR]"; break;
-    	    		case 3: prefix += "[ERROR]"; break;
-    	    		case 2: prefix += "[WARNING]"; break;
-    	    		case 1: prefix += "[INFO]"; break;
-    	    	}
-    	    	std::cout << ((logWithTime)? prefix+getSysTime()+buf : prefix+buf) << ((coloringLinuxConsole)? "\033[0m":"") << std::endl;
-    	    	return 0;
-    	    }
-    	    void logWithTheTime(bool option) {
-    	    	logWithTime = option;
-    	    }
-    	    void coloringOnLinux(bool option) {
-    	    	coloringLinuxConsole = option;
-    	    }
-    } io;
     class cmdContainer {
     private:
         argsAry args;
     public:
-        cmdContainer operator=(const std::string& str) {
+        cmdContainer USE_DLL operator=(const std::string& str) {
             this->args = compile(str);
             return this->args;
         }
-        cmdContainer() {
+        USE_DLL cmdContainer() {
 
         }
-        cmdContainer(std::string str) {
+        USE_DLL cmdContainer(std::string str) {
             args = compile(str);
         }
-        cmdContainer(argsAry argy) {
+        USE_DLL cmdContainer(argsAry argy) {
             args = argy;
         }
-        int run() {
-            if (cmd_register.count(args[0]) == 1) {
-                if (args.size() >= cmd_register[args[0]].argcMin && args.size() <= cmd_register[args[0]].argcMax) {
-                    return cmd_register[args[0]].func(args);
+        int USE_DLL run() {
+            if (cmd_register.count(args[0]) == 1) { //if command exist
+                if (args.size() >= cmd_register[args[0]].argcMin && args.size() <= cmd_register[args[0]].argcMax) { //passed the arguments check
+                    return cmd_register[args[0]].func(args);    //run the command
                 }
-                else {
-                	std::stringstream buf;
-                    buf << "Incorrect arguments count: needed ("
-                        << cmd_register[args[0]].argcMin << "," << cmd_register[args[0]].argcMax
-                        << ") but detected " << args.size();
-                    io.printWithPrefix(buf.str(), io.ERROR);
-                    return 32767;
+                else {  //can not pass the check
+                    return ARGUMENT_ERROR;
                 }
             }
-            else {
-            	std::stringstream buf;
-                buf << "Unknown command '" << args[0] << "'";
-                io.printWithPrefix(buf.str(), io.ERROR);
-                return 32768;
+            else {  //command not exist
+                return UNKNOWN_COMMAND;
             }
         }
     };
 
-    void regcmd(std::string cmdstr, //root command
+    void USE_DLL regcmd(std::string cmdstr, //root command
         Fp cmdfp,           //function pointer
         int argcMin,        //min argument count
         int argcMax) {      //max argument count
-        
+
         tCmdreg temp{ cmdfp,argcMin,argcMax };
         cmd_register[cmdstr] = temp;
     }
